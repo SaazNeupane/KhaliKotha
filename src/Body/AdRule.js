d3a6c9e4c3073c9a438352fc4bf9e94a724c3ea3
Saaz <saazneu789@gmail.com>
2021-09-04 12:13:41 +0545

    Contact Us, Rules and terms and Condition page

---
+++ src/Body/AdRule.js
@@ -0,0 +1,66 @@
+import { Component } from "react";
+
+class AdRule extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <header className="jumbotron my-4">
+                        <a className="logo" href="/">
+                            <img src={"./LogoBlack.png"} width="300" height="250" alt="logo" className="rounded mx-auto d-block" />
+                        </a>
+                        <b className="lead">
+                        <h2><b><u><center>Ad Posting Rules</center></u></b></h2></b>
+
+                        <b>Hamrobazar does not allow posting of ads that are considered illegal as per Nepalese law and/or are not permitted as per below Ad posting rules. Any user found violating these rules will be subjected to action/s mentioned below.</b>
+<br/><br/>
+1. Multiple (duplicate/repeat) ad posting.<br/>
+Users are not allowed to post more than 1 (one) similar active ad. Similar active ad are defined as ads selling or looking for same product or service, even if these ad contain different text or images and/or is posted from same or different account by a user.<br/>
+<br/>
+Each user is only allowed to post an ad similar to his/her previous ad if the previous one has been marked as sold, has been deleted or has already expired.
+<br/>
+2. Misleading and fraudulent ads.<br/>
+Some of the ads (but not limited) which fall under this are:<br/>
+<br/>
+    Ads title, price and content must be related. For e.g. posting ad with title Iphone 5 with irrelevant price Rs. 200 is not allowed.<br/>
+    Ads title should not include Price or Phone numbers, there is separate field for that.<br/>
+    Ads posted with marketing gimmick words with sole purpose of grabbing other user attention. For e.g. posting ad "Best opportunity of your life", "Click this to earn money", "Call me soon", "Don't miss out" etc.<br/>
+    Ads with Inaccurate/wrong prices. You need to clearly mention the price in your ad. For e.g. posting ad with price Rs. 5,000 but when later contacted you are asking another price.<br/>
+    Ads with Incorrect/wrong item condition mentioned. For e.g. posting used goods as Brand New, not disclosing defectiveness of goods.<br/>
+    Ads selling stolen and illegal goods.<br/>
+    Ads selling fake, replica and counterfeited goods by representing them as original.<br/>
+    Ads with sole purpose of leading user to another website or generate downloads without actual sale of product or service.<br/>
+    Ads which have Images unrelated with the ad content. Posting graphic (violent or adult oriented) images are not allowed.<br/>
+
+    <br/>
+3. Mis-categorized (wrong category) ads.<br/>
+Ads must be posted in the correct category. For e.g. posting a Bike ad in Mobile category, or office table in Home Furniture category is considered as mis-categorized ad. If you are unsure which category your ads belongs to, kindly search for existing ads to know where other users have posted similar ad. If your ad falls under more than one category, select the best and post your ad only in that category.
+<br/><br/>
+4. Posting illegal or morally obscene ads.<br/>
+Hamrobazar does not allow ads which are illegal as per Nepal law or are considered morally obscene in context to Nepalese society. Some of the ads (but not limited to) which fall under are:
+<br/>
+    Ads for Prostitution, obscene items or any form of adult entertainment ads like escort, indecent massages, pornography, sexual items, etc.<br/>
+    Ads which hurt the sentiments of any person, group or religion.<br/>
+    Ads selling illegal items like weapons, drugs, human organs, endangered animals, prohibited antiques, black marketing, etc.<br/>
+    Ads which infringes other third parties intellectual property, moral right and privacy.<br/>
+    <br/>
+5. Pyramid, referral marketing and other duping nature ads.<br/>
+Any ads (even genuine) which promote scheme which are of pyramid in nature and operate on basis of referrals. Such ads usually ask other user to join a program either to earn money or gift with end purpose of duping the user.<br/>
+
+<br/>
+<b>Consequences on Violation of Ad Posting Rules.</b><br/>
+Users who violate ad posting rules and site's terms of use may be subjected to any of the following actions, as per seriousness of violation deemed by us:<br/>
+1. Warning from site admin about the violation.<br/>
+2. Deletion of ads with/without prior notice.<br/>
+3. You maybe asked to provide identification documents.<br/>
+4. Temporary or Permanent blocking of account/access to the site.<br/>
+5. Reporting to Law Enforcement agency or Appropriate Authorities.<br/>
+
+
+                    </header>
+                </div>
+            </div>
+        )
+    }
+}
+export default AdRule;
---
+++ src/Body/Contactus.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="contact">
+                <div class="container">
+                    <div class="form">
+                        <div class="contact-info">
+                            <h3 class="title">Let's get in touch</h3>
+                            <p class="text"> Contact us with the following details. </p>
+                            <div class="info">
+                                <div class="social-information"> <i class="fa fa-map-marker"></i>
+                                    <p>Koteshwor-32,Kathmandu</p>
+                                </div>
+                                <div class="social-information"> <i class="fa fa-envelope-o"></i>
+                                    <p>kothakhali@gmail.com</p>
+                                </div>
+                                <div class="social-information"> <i class="fa fa-mobile-phone"></i>
+                                    <p>+977-9861320771 </p>
+                                </div>
+                            </div>
+                            <div class="social-media">
+                                <p>Connect with us:</p>
+                                <div class="social-icons"> <a href="/#"> <i class="fa fa-facebook-f"></i> </a> <a href="/#"> <i class="fa fa-twitter"></i> </a> <a href="/#"> <i class="fa fa-instagram"></i> </a> </div>
+                            </div>
+                        </div>
+                        <iframe title="Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14133.192417652861!2d85.33556683068089!3d27.677179874868894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19f2804a02bf%3A0x85468199859b2d8d!2sKoteshwor%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1617721418524!5m2!1sen!2snp" width="400" height="300" styles="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Contact;
